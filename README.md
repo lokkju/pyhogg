849b4d8f1575d30b47151b767f74630f7ba37699
Loki Coyote <lokkju@gmail.com>
2019-12-18 20:11:04 -0800

    adding missing generated code

--- README.md
+++ README.md
@@ -19,3 +19,7 @@ Lists and extracts files from Neverwinter Online mod 17, with correct filenames
 ## Data structure definitions
 All datastructures (hogg files, datalist, etc) are currently being defined using Kaitai; we then generate python code and wrap it.
 
+If you modify the ksy files, you must regenerate the python code:
+```
+kaitai-struct-compiler -t python kaitai/*.ksy
+```
---
+++ datalist_spec.py
@@ -0,0 +1,50 @@
+# This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+from pkg_resources import parse_version
+from kaitaistruct import __version__ as ks_version, KaitaiStruct, KaitaiStream, BytesIO
+
+
+if parse_version(ks_version) < parse_version('0.7'):
+    raise Exception("Incompatible Kaitai Struct Python API: 0.7 or later is required, but you have %s" % (ks_version))
+
+class DatalistSpec(KaitaiStruct):
+    def __init__(self, _io, _parent=None, _root=None):
+        self._io = _io
+        self._parent = _parent
+        self._root = _root if _root else self
+        self._read()
+
+    def _read(self):
+        self.dl_header = self._root.DatalistHeader(self._io, self, self._root)
+        self.entries = []
+        i = 0
+        while not self._io.is_eof():
+            self.entries.append(self._root.DatalistEntry(self._io, self, self._root))
+            i += 1
+
+
+    class DatalistHeader(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.version = self._io.read_u4le()
+            self.num_entries = self._io.read_u4le()
+
+
+    class DatalistEntry(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.data_size = self._io.read_u4le()
+            self.data = self._io.read_bytes(self.data_size)
+
+
+
